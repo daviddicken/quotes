d4946f2a51edf32f273535977ff26f78eb567c5b
daviddicken <davidfromseattle@gmail.com>
2020-09-24 00:33:25 -0700

    tried to figure out some test

--- src/test/java/quotes/AppTest.java
+++ src/test/java/quotes/AppTest.java
@@ -7,8 +7,5 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+    //crap I don't know how I would go about writing test for this app....
 }
